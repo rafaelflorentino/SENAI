72c8ebb1dd328cba16000f90a09a423acd2273bb
Rafael <enterrafael@gmail.com>
2023-08-03 20:41:07 -0300

    Ajuste header

--- PetShop/js/script.js
+++ PetShop/js/script.js
@@ -1,1 +1,39 @@
 /* Javascript */
+
+/* Abrir e Fechar mennu Mobile*/
+
+window.onload = function () {
+    /* Abrir Menu mobile*/
+    let btnMenu = document.getElementById("botao-menu");
+    btnMenu.onclick = function () {
+        menu.style.opacity= "1";    
+        menu.style.transition="0.5s";  
+        menu.style.transform="translateX(0rem)";
+    };
+    /* Fechar Menu mobile*/
+    let btnFechar = document.getElementById("btn-close");
+    let menu = document.getElementById("menu-mobile");
+    let corpo = document.getElementById("corpo");
+
+    btnFechar.onclick = function () {
+        menu.style.transition="0.5s";  
+        menu.style.transform="translateX(40rem)";
+        menu.style.opacity= "0";  
+    };
+
+    /* Fechar o menu depois de ser clicado */
+    menu.onclick = function () {
+        menu.style.transition="0.5s";  
+        menu.style.transform="translateX(40rem)";
+        menu.style.opacity= "0";  
+    };
+
+     /* Fechar o menu se clicar fora dele */
+     corpo.onclick = function () {
+        menu.style.transition="0.5s";  
+        menu.style.transform="translateX(40rem)";
+        menu.style.opacity= "0";  
+    };
+    
+
+}
